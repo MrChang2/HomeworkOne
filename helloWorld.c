686d1343652a4bdfdce4ca398decc647da789ae7
Matthew Chang <mchang246@gmail.com>
2019-01-27 16:55:21 -0800

    reverting

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main()
 {
+<<<<<<< HEAD
 	printf("Goodbye, I finished my CS2560 HW1.");
+=======
+	printf("Hello, World!");
+>>>>>>> parent of 130e6b5... second commit
 	return 0;
 }
