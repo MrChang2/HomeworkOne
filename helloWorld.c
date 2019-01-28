639c8a7897d5288e0b7c5fc8ab2b6c5beca16998
Matthew Chang <mchang246@gmail.com>
2019-01-27 16:19:20 -0800

    first commit

--- helloWorld.c
+++ helloWorld.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main()
 {
-<<<<<<< HEAD
-	printf("Goodbye, I finished my CS 2560 HW1");
-=======
 	printf("Hello, World!");
->>>>>>> parent of e070cca... second commit
 	return 0;
 }
